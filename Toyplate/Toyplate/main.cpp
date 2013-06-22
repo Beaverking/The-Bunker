8d6cf74cbdcd1bc931677a5aecebad664d814f64
Beaverking <Beaverking@Maxs-macbook-4.local>
2013-06-22 15:27:29 +0200

    Adding files

---
+++ Toyplate/Toyplate/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Toyplate
+//
+//  Created by Max on 6/22/13.
+//  Copyright (c) 2013 Max. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
